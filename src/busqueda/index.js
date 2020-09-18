631eaa01fbcc05d1d1cde9aca0fdbbbdffdcfac9
andres castillo <andcastillo@gmail.com>
2020-09-18 11:51:28 -0500

    add uniform cost search problem

---
+++ src/busqueda/index.js
@@ -0,0 +1,26 @@
+let maze = [[2, 1, 1], [2, 3, 3], [2, 3, 1], [1, 1, 1]]
+let start = [3, 0]
+let goal = [0, 2]
+const OPERATORS = ["U", "D", "L", "R"] // Priotity in case
+
+let problem = { maze, goal }
+
+// Avoid come back
+// root: {pos: [3, 0], cost: 0, parent: null, action: null}
+// node: {pos: [x, y], cost: number, parent: node, action: string}
+function testGoal(node, problem) {
+    if (node.pos[0] == problem.goal[0] && node.pos[1] == problem.goal[1]) {
+        return true;
+    }
+    return false;
+}
+
+function solve(problem, root) {
+    let solution = [];
+    // START CODE HERE
+
+    // END CODE HERE
+    return solution
+}
+
+console.log(solve(problem, start));
